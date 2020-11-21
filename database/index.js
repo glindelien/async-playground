dbebfd9a2ed3c93a76fdb4efe16900290f9d4636
Grace Lindelien <gracelindelien@gmail.com>
2020-11-21 10:49:15 -0800

    Create MySQL connection

---
+++ database/index.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql');
+const config = require('./config.js');
+
+const connection = mysql.createConnection(config);
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL!');
+});
