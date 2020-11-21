5051d441216b001f285d46e9ee77333ed4e6e220
Grace Lindelien <gracelindelien@gmail.com>
2020-11-21 12:24:00 -0800

    Refactor getAllTasks to be a Promise

--- database/queryHandlers.js
+++ database/queryHandlers.js
@@ -1,17 +1,33 @@
 const db = require('./index.js');
 
 const getAllTasks = (callback) => {
-  const queryStr = 'SELECT * FROM todos';
-  db.query(queryStr, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Data: ', data);
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM todos';
+    db.query(sql, (err, data) => {
+      if (err) {
+        return reject(err);
+      } else {
+        resolve(data);
+      }
       db.end(() => {
         console.log('MySQL Connection Closed');
       });
-    }
+    });
   });
+
+  // ERROR FIRST CALLBACK PATTERN
+
+  // db.query(queryStr, (err, data) => {
+  //   if (err) {
+  //     console.log(err);
+  //   } else {
+  //     console.log('Data: ', data);
+  //     db.end(() => {
+  //       console.log('MySQL Connection Closed');
+  //     });
+  //   }
+  // });
+
 };
 
 module.exports = {
