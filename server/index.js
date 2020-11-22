3102fd576d38d5ffc8cc565b453d4b1d5b799a0c
Grace Lindelien <gracelindelien@gmail.com>
2020-11-21 16:09:11 -0800

    Implement GET route for todos

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const db = require('../database/queryHandlers.js');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+app.get('/api/todos', (req, res) => {
+  db.getAllTasks()
+    .then((todos) => {
+      console.log('Sending Tasks to Client!');
+      res.send(todos);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+});
 
 app.listen(port, () => {
   console.log(`Async Playground listening at http://localhost:${port}`);
