904656354166b99338b42dd6740b90994975abbf
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 00:03:41 -0800

    Create zip code input field

---
+++ client/components/SubmitZipCode.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SubmitZipCode = () => (
+  <div class="submit-zip">
+    <form>
+      <label>
+        Zip Code&nbsp;&nbsp;
+      <input type="text" name="zip-code" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
+);
+
+export default SubmitZipCode;
