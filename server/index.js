6fdb01b9625375b85cb979952f73cc1da11180d9
Grace Lindelien <gracelindelien@gmail.com>
2020-12-10 23:49:43 -0800

    Use body-parser json middleware

--- server/index.js
+++ server/index.js
@@ -1,22 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const db = require('../database/queryHandlers.js');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/api/todos', (req, res) => {
-  db.getAllTasks()
-    .then((todos) => {
-      console.log('Sending Tasks to Client!');
-      res.status(200);
-      res.send(todos);
-    })
-    .catch((err) => {
-      res.status(500);
-      res.send();
-    })
+app.post('/api/getAqiByZip', (req, res) => {
+  console.log(req.body);
+  res.send();
 });
 
 app.listen(port, () => {
