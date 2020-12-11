e946b62346fdb29194656a852d311ec05bb160e4
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 00:03:56 -0800

    Render SubmitZipCode component in App

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Title from './components/Title.jsx';
+import SubmitZipCode from './components/SubmitZipCode.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class App extends React.Component {
     return (
       <div id='main'>
         <Title />
+        <SubmitZipCode />
       </div>
     );
   }
