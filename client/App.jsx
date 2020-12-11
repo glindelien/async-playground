4da6fe44efb3124cc1f6daa58cf6fac25384917d
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 00:21:38 -0800

    Implement handleInputChange method in App

--- client/App.jsx
+++ client/App.jsx
@@ -6,12 +6,27 @@ import SubmitZipCode from './components/SubmitZipCode.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      zipCode: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
+
+  handleInputChange(e) {
+    const zipCode = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: zipCode
+    });
+  }
+
   render() {
+    console.log(this.state.zipCode)
     return (
       <div id='main'>
         <Title />
-        <SubmitZipCode />
+        <SubmitZipCode handleInputChange={this.handleInputChange}
+                       zipCode={this.state.zipCode} />
       </div>
     );
   }
