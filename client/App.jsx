76fa203f86694846fbaf9a46a8854487f99a091e
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 00:29:30 -0800

    Implement handleSubmit method

--- client/App.jsx
+++ client/App.jsx
@@ -7,18 +7,23 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      zipCode: ''
+      zipCode: null
     };
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(e) {
-    e.preventDefault();
     const value = e.target.value;
     const name = e.target.name;
     this.setState({
       [name]: value
     });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target)
   }
 
   render() {
@@ -27,6 +32,7 @@ class App extends React.Component {
       <div id='main'>
         <Title />
         <SubmitZipCode handleInputChange={this.handleInputChange}
+                       handleSubmit={this.handleSubmit}
                        zipCode={this.state.zipCode} />
       </div>
     );
