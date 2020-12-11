926429ccf59977355c7a38374d82aa7930b46229
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 00:22:00 -0800

    Add handleInputChange method to input onChange

--- client/components/SubmitZipCode.jsx
+++ client/components/SubmitZipCode.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const SubmitZipCode = () => (
+const SubmitZipCode = ({ handleInputChange, zipCode }) => (
   <div class="submit-zip">
     <form>
       <label>
         Zip Code&nbsp;&nbsp;
-      <input type="text" name="zip-code" />
+      <input type="number"
+             name="zipCode"
+             onChange={handleInputChange}
+             value={zipCode} />
       </label>
       <input type="submit" value="Submit" />
     </form>
