0a42557e0607a3cbf764d13a96782eb46ea2f186
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 14:47:57 -0800

    Dynamically render air quality data from App state

--- client/components/AirQualityView.jsx
+++ client/components/AirQualityView.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const AirQualityView = () => (
-  <div id='air-quality-view'>
-    <div className='aqi ozone'>O3</div>
-    <div className='aqi PM25'>PM2.5</div>
-    <div className='aqi PM10'>PM10</div>
-  </div>
-);
+const AirQualityView = ({ ozone, pm25, pm10 }) => {
+  return (
+    <div id='air-quality-view'>
+      <div className='aqi ozone'>Ozone<br />{ozone}</div>
+      <div className='aqi PM25'>PM2.5<br />{pm25}</div>
+      <div className='aqi PM10'>PM10<br />{pm10}</div>
+    </div>
+  );
+};
 
 export default AirQualityView;
