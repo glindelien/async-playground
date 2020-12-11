4d5edb1e8f642cd9c5170bb08174b22ed3ebb8c1
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 11:49:10 -0800

    Build skeleton for air quality data

--- client/components/AirQualityView.jsx
+++ client/components/AirQualityView.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const AirQualityView = () => (
-  <div></div>
+  <div id='air-quality-view'>
+    <div class='aqi ozone'>O3</div>
+    <div class='aqi PM25'>PM2.5</div>
+    <div class='aqi PM10'>PM10</div>
+  </div>
 );
 
 export default AirQualityView;
