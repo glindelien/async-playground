bf0f40dd70fa745f0b2d71b1ee02fc4d881ce9d0
Grace Lindelien <gracelindelien@gmail.com>
2020-12-11 11:02:48 -0800

    Fetch AQI data from AirNow when user submits zip code

--- server/index.js
+++ server/index.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
+// Config Files
+const API_KEY = require('./airNowApiKey.js');
 const db = require('../database/queryHandlers.js');
 
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+// Endpoints
+const airNowURL = 'https://airnowapi.org';
+const currentAqi = '/aq/observation/zipCode/current';
+
 app.post('/api/getAqiByZip', (req, res) => {
-  console.log(req.body);
-  res.send();
+  // Extract user inputted zip code
+  const zipCode = req.body.zipCode;
+  const params = `?zipCode=${zipCode}&api_key=${API_KEY}&format=JSON`;
+  // Fetch AQI data from AirNow API
+  axios.get(airNowURL + currentAqi + params)
+  .then((result) => {
+    console.log(result.data);
+    res.send();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Awaire listening at http://localhost:${port}`);
+  console.log(`Awaire is listening at http://localhost:${port}`);
 });
