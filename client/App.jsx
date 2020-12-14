f22e9cf2c6f8b29d49d9b3653e5a311658826199
Grace Lindelien <gracelindelien@gmail.com>
2020-12-14 14:25:39 -0800

    Lift state into App, render form in App instead of AirQualityView

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
+import axios from 'axios';
 
 import Title from './components/Title.jsx';
+import SubmitZipCode from './components/SubmitZipCode.jsx';
 import AirQualityView from './components/AirQualityView.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      zipCode: '',
+      location: '',
+      aqi: []
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post('/api/getAqiByZip', {
+      zipCode: this.state.zipCode
+    })
+      .then((res) => {
+        const city = res.data[0].ReportingArea;
+        const state = res.data[0].StateCode;
+        this.setState({
+          zipCode: '',
+          location: `${city}, ${state}`,
+          aqi: res.data
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <div id='main'>
         <Title />
-        <AirQualityView />
+        <SubmitZipCode handleInputChange={this.handleInputChange}
+                       handleSubmit={this.handleSubmit}
+                       zipCode={this.state.zipCode} />
+        <AirQualityView location={this.state.location}
+                        aqi={this.state.aqi}
+                        handleInputChange={this.handleInputChange}
+                        handleSubmit={this.handleSubmit}
+                        zipCode={this.state.zipCode} />
       </div>
     );
   }
--- client/components/AirQualityView.jsx
+++ client/components/AirQualityView.jsx
@@ -1,57 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 
-import SubmitZipCode from './SubmitZipCode.jsx';
 import Location from './Location.jsx';
 import AirQualityCard from './AirQualityCard.jsx';
 
-class AirQualityView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zipCode: '',
-      location: '',
-      AQI: []
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const AirQualityView = ({location, aqi, zipCode }) => {
 
-  handleInputChange(e) {
-    const value = e.target.value;
-    const name = e.target.name;
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    axios.post('/api/getAqiByZip', {
-      zipCode: this.state.zipCode
-    })
-      .then((res) => {
-        const city = res.data[0].ReportingArea;
-        const state = res.data[0].StateCode;
-        this.setState({
-          zipCode: '',
-          location: `${city}, ${state}`,
-          AQI: res.data
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  render() {
     let locationJSX = null;
     let airQualityCardJSX = null;
 
     // Only render these components if user submits zip code
-    if (this.state.location !== '') {
-      locationJSX = <Location location={this.state.location}/>;
-      airQualityCardJSX = this.state.AQI.map((data, index) => {
+    if (location !== '') {
+      locationJSX = <Location location={location}/>;
+      airQualityCardJSX = aqi.map((data, index) => {
         return (
           <AirQualityCard data={data}
                           key={index} />
@@ -60,16 +20,12 @@ class AirQualityView extends React.Component {
 
     return (
       <div>
-        <SubmitZipCode handleInputChange={this.handleInputChange}
-                       handleSubmit={this.handleSubmit}
-                       zipCode={this.state.zipCode} />
         {locationJSX}
         <div id='aqi-cards'>
           {airQualityCardJSX}
         </div>
       </div>
     );
-  }
 };
 
 export default AirQualityView;
