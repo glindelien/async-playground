5356658ed95f9f1b8dfaf25c9ed8b3ed5f853b6a
Grace Lindelien <gracelindelien@gmail.com>
2021-01-12 14:11:25 -0800

    Render button to clear localStorage if AQI data exists

--- client/components/AirQualityView.jsx
+++ client/components/AirQualityView.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 
 import AirQualityCard from './AirQualityCard.jsx';
 
-const AirQualityView = ({ aqi }) => (
-  <div className="aqi-cards">
-    {aqi.map((location, index) => {
-      return (
-        <AirQualityCard location={location} key={index} />
-      )
-    })}
-  </div>
-);
+const AirQualityView = ({ aqi, clearLocalStorage }) => {
+  let removeLocations = null;
+  if (aqi.length) {
+    removeLocations = (
+      <div onClick={clearLocalStorage}>
+        <span id="clear-storage">Remove All Locations</span>
+      </div>);
+  }
+  return (
+    <div className="air-quality-view">
+      <div className="aqi-cards">
+        {aqi.map((location, index) => {
+          return (
+            <AirQualityCard location={location} key={index} />
+          )
+        })}
+      </div>
+      {removeLocations}
+    </div>
+  );
+};
 
 export default AirQualityView;
