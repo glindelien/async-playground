fbdb0ed46da3744c8e9ab54564f4b62628e8b6ca
Grace Lindelien <gracelindelien@gmail.com>
2021-01-15 15:57:34 -0800

    Implement delete button to remove individual locations

--- client/components/airQualityView/airQualityCard/AirQualityCard.jsx
+++ client/components/airQualityView/airQualityCard/AirQualityCard.jsx
@@ -3,37 +3,60 @@ import React from 'react';
 import Location from './Location.jsx';
 import IndexCard from './IndexCard.jsx';
 
-const AirQualityCard = ({ location, removeLocation }) => {
-
-  let locationJSX = null;
-  let airQualityCardJSX = null;
-
-  // Only render these components if user added location(s)
-  if (location.length > 0) {
-    const city = location[0].ReportingArea;
-    const state = location[0].StateCode;
-    locationJSX = <Location loc={`${city}, ${state}`} />
-
-    airQualityCardJSX = location.map((data, index) => {
-      return (
-          <IndexCard data={data}
-                     key={index} />
-      )
-    });
+class AirQualityCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showButton: false
+    }
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
-  return (
-    <div className="aqi-card"
-         zipcode={location[0].ZipCode}
-         onClick={removeLocation}>
-      {/* Display Location for AQI Card */}
-      {locationJSX}
-      {/* Display AQI data for location */}
-      <div className="index-cards">
-        {airQualityCardJSX}
-      </div>
-    </div>
-  );
-};
+  onMouseEnter(e) {
+    this.setState({ showButton: true });
+  }
+
+  onMouseLeave() {
+    this.setState({ showButton: false });
+  }
+
+  render() {
+    const { location, removeLocation } = this.props;
+    let locationJSX = null;
+    let airQualityCardJSX = null;
+    let loc = <span className="delete">X</span>;
+
+    // Only render these components if user added location(s)
+    if (location.length > 0) {
+      if (!this.state.showButton) {
+        loc = `${location[0].ReportingArea}, ${location[0].StateCode}`;
+      }
+      locationJSX = <Location loc={loc}
+                              onMouseEnter={this.onMouseEnter}
+                              onMouseLeave={this.onMouseLeave}
+                              removeLocation={removeLocation}
+                              zipcode={location[0].ZipCode} />
+
+      airQualityCardJSX = location.map((data, index) => {
+        return (
+          <IndexCard data={data}
+                     key={index} />
+        )
+      });
+
+      return (
+        <div className="aqi-card">
+          {/* Display Location for AQI Card */}
+          {locationJSX}
+          {/* Display AQI data for location */}
+          <div className="index-cards">
+            {airQualityCardJSX}
+          </div>
+        </div>
+      );
+    }
+  }
+}
 
 export default AirQualityCard;
--- client/components/airQualityView/airQualityCard/Location.jsx
+++ client/components/airQualityView/airQualityCard/Location.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Location = ({ loc }) => (
-  <div className='location'>
-    {loc}
-  </div>
-);
+const Location = ({ loc, onMouseEnter, onMouseLeave, removeLocation, zipcode }) => {
+  return (
+    <div className="location"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onClick={removeLocation}
+        zipcode={zipcode} >
+      {loc}
+    </div>
+  );
+};
 
 export default Location;
