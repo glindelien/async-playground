56c430c633df8ef3d0e540d70ad250a7fb48e43e
Grace Lindelien <gracelindelien@gmail.com>
2021-03-23 16:15:09 -0700

    Make 'Remove All Locations' button focusable'

--- client/components/airQualityView/AirQualityView.jsx
+++ client/components/airQualityView/AirQualityView.jsx
@@ -6,8 +6,9 @@ const AirQualityView = ({ aqi, removeLocation, clearLocalStorage }) => {
   let removeAllLocationsButton = null;
   if (aqi.length) {
     removeAllLocationsButton = (
-      <div onClick={clearLocalStorage}>
-        <span id="clear-storage">Remove All Locations</span>
+      <div onClick={clearLocalStorage}
+           onKeyDown={clearLocalStorage} >
+        <span tabIndex="0" role="button" id="clear-storage">Remove All Locations</span>
       </div>);
   }
   return (
