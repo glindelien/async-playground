604ad12ae80c3b9305b7bd224d3f4f2d66ebf870
Grace Lindelien <gracelindelien@gmail.com>
2021-06-15 11:04:54 -0700

    Create NavBar component skeleton

---
+++ client/components/navBar/navBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+      <table id="nav-table">
+        <tr>
+          <td>Home</td>
+          <td>About</td>
+          <td>AQI Legend</td>
+        </tr>
+      </table>
+  );
+};
+
+export default NavBar;
