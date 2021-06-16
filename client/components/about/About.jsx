9c201396d3379d98a81f5195f08cdd84c6295c2b
Grace Lindelien <gracelindelien@gmail.com>
2021-06-16 14:23:16 -0700

    Write copy for About component

--- client/components/about/About.jsx
+++ client/components/about/About.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const About = () => (
-  <span>Description of Awaire App Here</span>
+  <div id="about">
+    <p>Awaire is a simple app designed to provide instant insight into the current air quality information for locations in the United States. Simply add a location by zip code, and track current AQI data for multiple saved locations.</p>
+    <p>Awaire is a handy tool for anyone who lives in areas prone to wildfires, commuters traveling through polluted areas, travelers planning their routes, or even those who just want to check in on a family member in their home city.</p>
+  </div>
 );
 
 export default About;
