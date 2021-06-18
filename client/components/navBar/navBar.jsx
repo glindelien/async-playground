a9fa8a2c0b1396b1e1050b6da757ff470f0c10e5
Grace Lindelien <gracelindelien@gmail.com>
2021-06-17 17:01:21 -0700

    Wrap NavBar in nav element

--- client/components/navBar/navBar.jsx
+++ client/components/navBar/navBar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const NavBar = ({ switchView }) => {
   return (
+    <nav>
       <table id="nav-table">
         <tr>
           <td onClick={switchView}>Home</td>
           <td onClick={switchView}>About</td>
         </tr>
       </table>
+    </nav>
   );
 };
 
